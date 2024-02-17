8e5273c1972fe829c603084d55922b5ba77b7bc0
Saed hasan <saed.it.cis@gmail.com>
2024-02-18 00:21:42 +0300

    edit jenkins file

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "hello Tim Lam trigger the pipeline1" + "</h1>");
+		out.println("<h1>" + "hello Tim Lam trigger the pipeline" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
