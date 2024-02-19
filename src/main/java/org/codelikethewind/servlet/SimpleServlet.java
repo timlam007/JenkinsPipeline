d1b2df610db3098ca8dbe01017ff320aec90d661
Tim Lam <89241319+timlam007@users.noreply.github.com>
2024-02-19 21:12:54 +0800

    Update SimpleServlet.java

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "hello Tim Lam trigger the pipeline from tim github1" + "</h1>");
+		out.println("<h1>" + "hello Tim Lam trigger the pipeline from my Github" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
